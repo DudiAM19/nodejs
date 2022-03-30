693fa610ae8d0d2ae59a770858bcfbd5822e3e5a
Dudi <dudi.murtado@ist.id>
2022-03-30 16:36:32 +0700

    update blog

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,5 +1,5 @@
 const {validationResult} = require('express-validator');
-const BlogPost = require('../models/blog')
+const BlogPost = require('../models/blog');
 
 exports.createBlog = (req,res, next) => {
     const error = validationResult(req);
@@ -47,5 +47,69 @@ exports.getBlog = (req, res, next) => {
     })
     .then(err => {
         next(err)
+    })
+}
+
+exports.getBlogById = (req, res, next) => {
+    const postId = req.params.postId;
+    BlogPost.findById(postId)
+    .then(result => {
+        if(!result){
+            const error = new Error('id tidak ditemukan');
+            error.errorStatus = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Data berhasil dipanggil',
+            data: result,
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+exports.updateBlog = (req, res, next) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        const err = new Error("invalid Value");
+        err.errorStatus = 400;
+        err.data = error.array();
+        throw err;
+    }
+
+    if(!req.file){
+        const err = new Error("Image harus diupload");
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post){
+            const err = new Error('Blog post tidak ditemukan');
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update Sukses!',
+            data: result,
+        })
+    })
+    .catch(err => {
+        next(err);
     })
 }
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -11,5 +11,11 @@ router.post('/post', [
     blogController.createBlog);
 
 router.get('/posts', blogController.getBlog);
+router.get('/post/:postId', blogController.getBlogById);
+router.put('/post/:postId', [
+    body('title').isLength({min: 5}).withMessage("input title anda kurang dari 5 huruf"),
+    body('body').isLength({min: 5}).withMessage("input body anda kurang dari 5 huruf")
+    ], 
+    blogController.updateBlog);
 
 module.exports = router;
